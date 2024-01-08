5714973791aa40e69e1bae0b390017ef3c5ed9da
Amandine Solignac <asolignac@enseirb-matmeca.fr>
2024-01-08 11:45:00 +0100

    refactoring V2 + fichiers annexes

--- mainV2.py
+++ mainV2.py
@@ -1,7 +1,8 @@
 from tkinter import *
 from PIL import Image, ImageTk  # Librairie à insaller : pip install pillow
 from carcassonne_structure import *
-
+from affichage import *
+from action_utilisateur import *
 
 def position_dans_tuile(x, y):
     """Renvoie la position x et y à l'intérieur de la tuile, en se basant sur une subdivision 3*3"""
@@ -10,100 +11,7 @@ def position_dans_tuile(x, y):
     return x, y
 
 
-def afficher_image_temporaire(canva, i, j):
-    """Affichage de l'image temporaire au niveau du curseur"""
-    global image_temporaire
-    if 0 <= i < grille_x_longueur and 0 <= j < grille_y_longueur:  # On se place dans la grille affichée
-        if jeu.plat[i + grille_x_offset][
-            j + grille_y_offset] == -1:  # Si on a aucune tuile à cet emplacement on montre la tuile actuelle
-            img = Image.open("images/tuile" + str(jeu.pioche[0]) + ".png")  # On charge l'image puis on l'affiche
-            img = img.rotate(jeu.sac_tuiles[jeu.pioche[0]].rotation * 90)
-            image_temporaire = ImageTk.PhotoImage(img)
-            afficher_image(canva, i * 100, j * 100, image_temporaire)
-        else:
-            image_temporaire = 0
-
-
-def curseur_dans_derniere_tuile_posee(x, y):
-    """True si le curseur se situe là où on a posé la dernière tuile"""
-    return (x // 100 + grille_x_offset == jeu.derniere_position[0] and
-            y // 100 + grille_y_offset == jeu.derniere_position[1])
-
-
-def afficher_meeple_temporaire(canva, position, x, y):
-    """Affichage du meeple temporaire"""
-    global image_temporaire_meeple
-    if curseur_dans_derniere_tuile_posee(x, y):  # si le curseur est sur la tuile qui vient d'être posée
-        x, y = position_dans_tuile(x, y)
-
-        if x == 1 or y == 1:  # le meeple est à un des emplacements qu'on souhaite donc on l'affiche
-            img_meeple = Image.open("images/meeple" + str(jeu.joueur_courant) + ".png")
-            image_temporaire_meeple = ImageTk.PhotoImage(img_meeple)
-            afficher_image_meeple(canva, [position[0], position[1], point_cardinal_curseur[(x, y)]],
-                                  image_temporaire_meeple)
-        else:
-            image_temporaire_meeple = 0
-    else:
-        image_temporaire_meeple = 0
-
-
-def afficher_image(canva, x, y, img):
-    """On affiche l'image dans le canvas"""
-    canva.create_image(x, y, anchor=NW, image=img)
-
-
-def afficher_grille(canva, tableau):
-    """On affiche une grille de n*m tuiles sur l'écran"""
-    canva.delete("all")  # quand on dessine les tuiles, on efface ce qu'on avait en dessous
-    for i in range(grille_x_longueur):
-        for j in range(grille_y_longueur):
-            if tableau[i + grille_x_offset][j + grille_y_offset] >= 0:  # si on a une tuile à afficher
-                afficher_image(canva, i * 100, j * 100, images[tableau[i + grille_x_offset][j + grille_y_offset]])
-            elif tableau[i + grille_x_offset][j + grille_y_offset] < -1:  # si on a un mur
-                afficher_image(canva, i * 100, j * 100, images[72])
-            else:  # on a de l'herbe
-                afficher_image(canva, i * 100, j * 100, images[73])
-                canva.create_text(i * 100 + 50, j * 100 + 50,
-                                  text="(" + str(i + grille_x_offset) + "," + str(j + grille_y_offset) + ")",
-                                  fill="white")  # DEBUG
-
-    if jeu.phase_courante == 1:  # on encadre la dernière tuile posée si on est en phase de meeple
-        afficher_encadrement_tuile(canva)
-
-
-def afficher_encadrement_tuile(canva):
-    """Affiche un cadre autour de la dernière tuile posée"""
-    x_pos_affichage = jeu.derniere_position[0] - grille_x_offset
-    y_pos_affichage = jeu.derniere_position[1] - grille_y_offset
-    if x_pos_affichage in range(grille_x_longueur) and y_pos_affichage in range(grille_y_longueur):
-        # on encadre la tuile qui vient d'être posée si elle est dans l'afficage actuel
-        canva.create_rectangle(x_pos_affichage * 100, y_pos_affichage * 100, (x_pos_affichage + 1) * 100,
-                               (y_pos_affichage + 1) * 100, outline="bisque", width=2)
-
-
-def afficher_image_meeple(canva, coords, img):
-    """On affiche un meeple en fonction de ses coordonnées x,y et point_cardinal"""
-    x_offset, y_offset = affichage_dans_la_tuile[coords[2]][0], affichage_dans_la_tuile[coords[2]][
-        1]  # au sein de la tuile, on décale selon le point cardinal
-    x_pos, y_pos = coords[0] - grille_x_offset, coords[1] - grille_y_offset  # calcul de la position de la tuile
-    canva.create_image(x_pos * 100 + x_offset, y_pos * 100 + y_offset, anchor=NW, image=img)
-
-
-def afficher_meeple(canva):
-    """Affiche tous les meeples des joueurs qui sont sur le plateau"""
-    for i in range(jeu.nb_joueurs):
-        print("Score du joueur", i + 1, ":", jeu.joueurs[i].score)
-        for j in range(len(jeu.joueurs[i].meeples)):
-            meeple = jeu.joueurs[i].meeples[j]
-            coords = meeple.coordonnees
-            if meeple.type != 0 and coords[0] - grille_x_offset in range(grille_x_longueur) \
-                    and coords[1] - grille_y_offset in range(
-                grille_y_longueur):  # on affiche le meeple s'il est dans la portion qu'on affiche et qu'il fait parti des meeples à afficher
-                afficher_image_meeple(canva, coords, images_meeple[i])
-            print(jeu.joueurs[i].meeples[j])
-
-
-def add_image_liste(id_image_ajoutee, rotation):
+def ajout_image_liste(id_image_ajoutee, rotation):
     """On ajoute la tuile à la liste d'images fixes"""
     img = Image.open("images/tuile" + str(id_image_ajoutee) + ".png")  # chargement de l'image
     img = img.rotate(rotation * 90)  # rotation de l'image
@@ -111,99 +19,7 @@ def add_image_liste(id_image_ajoutee, rotation):
     images[id_image_ajoutee] = img_sauvegardee
 
 
-def event_clic_gauche(event):
-    """Le joueur fait un clic gauche dans le canvas"""
-    global image_temporaire, image_temporaire_meeple
-    if not jeu.fini:
-        x, y = event.x // 100 + grille_x_offset, event.y // 100 + grille_y_offset
-
-        if jeu.phase_courante == 0:  # phase de tuile
-            if jeu.input_add_tile(x, y) == True:  # le joueur fait une proposition valide
-                add_image_liste(jeu.pioche[0], jeu.sac_tuiles[jeu.pioche[0]].rotation)  # ajout de l'image à la liste
-
-                jeu.fin_phase_tuile(x, y)  # on termine la phase
-
-                afficher_grille(canvas, jeu.plat)  # mise à jour de l'affichage
-                afficher_meeple(canvas)
-                image_temporaire = 0
-
-        elif jeu.phase_courante == 1:  # phase meeple
-            i, j = position_dans_tuile(event.x, event.y)  # i et j valent entre 0 et 2 au sein de la tuile subdivisée
-            if i == 1 or j == 1:  # Au sein de la tuile pointée, on se situe à un des endroits où le meeple peut être placé
-                point_cardinal = point_cardinal_curseur[(i, j)]
-                if jeu.check_pour_poser_meeple(x, y, point_cardinal) == True:  # le joueur fait une proposition valide
-
-                    jeu.fin_phase_meeple(point_cardinal)  # on termine la phase
-
-                    afficher_meeple(canvas)  # mise à jour de l'affichage
-                    image_temporaire_meeple = 0
-
-
-def event_clic_droit(event):
-    """Clic droit dans le canvas"""
-    global image_temporaire_meeple
-    if not jeu.fini:
-        if jeu.phase_courante == 0:  # phase de tuile
-            jeu.sac_tuiles[jeu.pioche[0]].tourner()
-            afficher_image_temporaire(canvas, event.x // 100, event.y // 100)
-
-        elif jeu.phase_courante == 1:
-            passage_au_joueur_suivant()
-            image_temporaire_meeple = 0
-
-
-def deplacement_curseur(event):
-    """Déplacement du curseur dans le canvas"""
-    if not jeu.fini:
-        if jeu.phase_courante == 0:  # phase de tuile
-            afficher_image_temporaire(canvas, event.x // 100, event.y // 100)  # on affiche la tuile temporaire
-        elif jeu.phase_courante == 1:  # phase de meeple
-            afficher_meeple_temporaire(canvas, jeu.derniere_position, event.x,
-                                       event.y)  # on affiche le meeple temporaire
-
-
-def passage_au_joueur_suivant():
-    jeu.joueur_suivant()
-    afficher_grille(canvas, jeu.plat)
-    afficher_meeple(canvas)
-
-
-def deplacement_grille_vers_la_gauche(event):
-    mise_a_jour_grille_quand_deplacement(-1, 0, canvas)
-
-
-def deplacement_grille_vers_la_droite(event):
-    mise_a_jour_grille_quand_deplacement(1, 0, canvas)
-
-
-def deplacement_grille_vers_le_haut(event):
-    mise_a_jour_grille_quand_deplacement(0, -1, canvas)
-
-
-def deplacement_grille_vers_le_bas(event):
-    mise_a_jour_grille_quand_deplacement(0, 1, canvas)
-
-
-def mise_a_jour_grille_quand_deplacement(modif_x, modif_y, canva):
-    global grille_x_offset, grille_y_offset
-    # on met à jour la partie affichée si on ne passe pas au dela d'un mur
-    if grille_x_offset + modif_x in range(len(jeu.plat) - grille_x_longueur + 1) and \
-            grille_y_offset + modif_y in range(len(jeu.plat) - grille_y_longueur + 1):
-        grille_x_offset += modif_x
-        grille_y_offset += modif_y
-        afficher_grille(canva, jeu.plat)
-        afficher_meeple(canva)
-
-
-def fin_jeu(event):
-    jeu.fin_du_jeu()
-
-
-def skip_tuile(event):
-    passage_au_joueur_suivant(jeu)
-
-
-def setup_offset_grille():
+def setup_camera_on_grille():
     """Place la caméra au centre du plateau"""
     global grille_x_offset, grille_y_offset
     grille_x_offset = (len(jeu.plat) - grille_x_longueur + 1) // 2
@@ -225,6 +41,7 @@ def interactions_utilisateurs():
 
     main_frame.bind("f", fin_jeu)
     main_frame.bind("s", skip_tuile)
+
 
 if __name__ == "__main__":
     Window = Tk()
@@ -279,7 +96,7 @@ if __name__ == "__main__":
 
     jeu = Jeu(2, 3)  # initialisation du jeu pour deux joueurs
 
-    setup_offset_grille()  # les coordonnées d'offset sont de telle sorte qu'on se place au milieu de la grille
+    setup_camera_on_grille()  # les coordonnées d'offset sont de telle sorte qu'on se place au milieu de la grille
     afficher_grille(canvas, jeu.plat)  # affichage de la grille au début du jeu
 
     canvas.pack()
